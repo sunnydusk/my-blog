fbedf94d9132bd1eb3741de474d191f729f153e1
sunnydusk <sunnyduskxi@outlook.com>
2023-10-26 09:41:39 +0800

    2.5

--- public/sw.js
+++ public/sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","db0496cf3ed101825276517463eb74ae"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
